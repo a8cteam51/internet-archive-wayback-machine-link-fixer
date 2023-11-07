656912d33d4312c1aa76379e5f858414249ec311
Glynn Quelch <glynn.quelch@gmail.com>
2023-11-07 12:18:12 +0000

    Updated the link checker to now use curl and follow redirects with a custom timeout setting

--- src/Report/Link.php
+++ src/Report/Link.php
@@ -15,13 +15,22 @@ namespace WPCOMSpecialProjects\Wayback_Link_Fixer\Report;
 class Link {
 
 	/**
+	 * The index/position of the link in the content.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var integer
+	 */
+	private int $index;
+
+	/**
 	 * The existing link href
 	 *
 	 * @since 1.0.0
 	 *
 	 * @var string
 	 */
-	private string $href;
+	private ?string $href;
 
 	/**
 	 * The links contents.
@@ -30,7 +39,7 @@ class Link {
 	 *
 	 * @var string
 	 */
-	private string $contents;
+	private ?string $contents;
 
 	/**
 	 * Is the link broken.
@@ -78,27 +87,41 @@ class Link {
 	private bool $fixed = false;
 
 	/**
+	 * Comments on the link
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string[]
+	 */
+	private array $comments = array();
+
+	/**
 	 * Create instance of Report_Logs_Link.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param string             $href                The link href.
-	 * @param string             $contents            The link contents.
+	 * @param integer            $index               The index/position of the link in the content.
+	 * @param string|null        $href                The link href.
+	 * @param string|null        $contents            The link contents.
 	 * @param boolean            $broken              Is the link broken.
 	 * @param string|null        $redirect_target     The link redirection target.
 	 * @param integer|null       $redirect_type       The link redirection type.
 	 * @param array<int, string> $replacement_options The link replacement options.
 	 * @param boolean            $fixed               Was the link fixed.
+	 * @param string[]           $comments            Comments on the link.
 	 */
 	public function __construct(
-		string $href,
-		string $contents,
+		int $index,
+		?string $href,
+		?string $contents,
 		bool $broken = false,
 		?string $redirect_target = null,
 		?int $redirect_type = null,
 		array $replacement_options = array(),
-		bool $fixed = false
+		bool $fixed = false,
+		array $comments = array()
 	) {
+		$this->index               = $index;
 		$this->href                = $href;
 		$this->contents            = $contents;
 		$this->broken              = $broken;
@@ -106,6 +129,18 @@ class Link {
 		$this->redirect_type       = $redirect_type;
 		$this->replacement_options = $replacement_options;
 		$this->fixed               = $fixed;
+		$this->comments            = $comments;
+	}
+
+	/**
+	 * Get the index/position of the link in the content.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return integer
+	 */
+	public function get_index(): int {
+		return $this->index;
 	}
 
 	/**
@@ -113,9 +148,9 @@ class Link {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return string
+	 * @return string|null
 	 */
-	public function get_href(): string {
+	public function get_href(): ?string {
 		return $this->href;
 	}
 
@@ -124,9 +159,9 @@ class Link {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return string
+	 * @return string|null
 	 */
-	public function get_contents(): string {
+	public function get_contents(): ?string {
 		return $this->contents;
 	}
 
@@ -183,5 +218,16 @@ class Link {
 	 */
 	public function is_fixed(): bool {
 		return $this->fixed;
+	}
+
+	/**
+	 * Get the comments on the link.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string[]
+	 */
+	public function get_comments(): array {
+		return $this->comments;
 	}
 }
