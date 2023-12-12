12ab9762b5c58b6e483ec30b5817bac7ce742591
Glynn Quelch <glynn.quelch@gmail.com>
2023-12-12 12:24:56 +0000

    composer install with no dev deps and optimised autoloader

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'cli' => array($vendorDir . '/wp-cli/php-cli-tools/lib'),
-    'WP_CLI\\' => array($vendorDir . '/wp-cli/wp-cli/php'),
-    'Mustache' => array($vendorDir . '/mustache/mustache/src'),
 );
