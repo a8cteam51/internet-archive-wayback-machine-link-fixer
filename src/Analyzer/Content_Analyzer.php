33b02de205f3e5524cd7eedd487e44e03f95ce94
Glynn Quelch <glynn.quelch@gmail.com>
2023-12-13 14:46:58 +0000

    House Keeping

--- src/Analyzer/Content_Analyzer.php
+++ src/Analyzer/Content_Analyzer.php
@@ -153,7 +153,7 @@ class Content_Analyzer {
 
 		// If the way back machine content is not set, get it.
 		if ( ! $this->way_back_machine_content ) {
-			$page_url = get_permalink( $this->post_id );
+			$page_url                       = get_permalink( $this->post_id );
 			$this->way_back_machine_content = $this->way_back_machine->get_content( $page_url );
 		}
 
