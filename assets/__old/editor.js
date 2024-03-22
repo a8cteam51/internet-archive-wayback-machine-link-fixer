735da5f6d04bc5b4061415d9f5fbcfe0855469d8
Glynn Quelch <glynn.quelch@gmail.com>
2024-03-22 14:09:51 +0000

    Remove the old code

--- assets/__old/editor.js
+++
@@ -1,9 +0,0 @@
-import { createHooks } from '@wordpress/hooks';
-import domReady from '@wordpress/dom-ready';
-
-window.wpcomsp_wayback_link_fixer = window.wpcomsp_wayback_link_fixer || {};
-window.wpcomsp_wayback_link_fixer.hooks = createHooks();
-
-domReady( () => {
-	window.wpcomsp_wayback_link_fixer.hooks.doAction( 'editor.ready' );
-} );
