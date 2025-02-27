3a7a23b6d333e3204cc1e6f2b4f126d353ed35ec
Glynn Quelch <glynn.quelch@gmail.com>
2025-02-27 09:15:02 +0000

    Change version number on plugin entry file

--- wayback-link-fixer.php
+++ wayback-link-fixer.php
@@ -3,7 +3,7 @@
  * The wayback-link-fixer bootstrap file.
  *
  * @since       1.0.0
- * @version     1.3.0RC1
+ * @version     1.3.0-RC1
  * @author      WordPress.com Special Projects
  * @license     GPL-3.0-or-later
  *
