3b577330b800d2db0ee4d418cef7c362494827ea
Glynn Quelch <glynn.quelch@gmail.com>
2025-04-01 09:36:47 +0100

    Share styles between wizard and setttings around margins and padding

--- templates/admin/wizard/header.php
+++ templates/admin/wizard/header.php
@@ -9,10 +9,8 @@
  */
 ?>
 
-<div id="wlf_wizard"> <!-- Wizard container -->
+<div id="wlf_wizard" class="wlf_settings_card"> <!-- Wizard container -->
 	<div class="wlf-wizard__content"> <!-- Wizard content -->
-		<h1><?php esc_html_e( 'Wayback Link Fixer Setup Wizard', 'wpcomsp_wayback_link_fixer' ); ?></h1>
-
 		<form method="post" action="">
 			<input type="hidden" name="wlf-action" value="start-wizard" />
 			<input type="hidden" name="wlf-current-step" value="<?php echo esc_attr( $step_data['step'] ); ?>" />
