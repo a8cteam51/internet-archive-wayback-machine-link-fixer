2c119629986e97219c4a6caa65618f6ba298480b
Glynn Quelch <glynn.quelch@gmail.com>
2025-06-25 09:59:28 +0100

    Now when the content scanner is run, it will ingore all links from the current site in either http or https protocals

--- src/Processor/Content_Scanner.php
+++ src/Processor/Content_Scanner.php
@@ -99,7 +99,7 @@ class Content_Scanner {
 		return array_filter(
 			$this->links,
 			function ( string $link ): bool {
-				return ! wpcomsp_wayback_link_fixer_is_archive_link( $link );
+				return ! wpcomsp_wayback_link_fixer_is_archive_link( $link ) && ! wpcomsp_wayback_link_fixer_is_current_site_link( $link );
 			}
 		);
 	}
